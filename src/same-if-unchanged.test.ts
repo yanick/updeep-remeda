b70dccc43fb8f73bf0787fcb1752fcf11aed51f0
Yanick Champoux <yanick@babyl.ca>
2023-03-22 17:00:43 -0400

    add test for idempotency

--- src/same-if-unchanged.test.ts
+++ src/same-if-unchanged.test.ts
@@ -20,4 +20,10 @@ describe("update", () => {
     const result = u(orig, { b: u.skip });
     expect(result).toBe(orig);
   });
+
+  test("found in the wild", () => {
+    const x = { a: { c: 3 }, b: 2 };
+    const res = u(x, { a: { c: 3 } });
+    expect(res).toBe(x);
+  });
 });
